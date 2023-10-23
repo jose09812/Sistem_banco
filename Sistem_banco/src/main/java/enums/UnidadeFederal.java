b7426307268ed0d8bcb73ac05933642c160d5743
jose09812 <142240210+jose09812@users.noreply.github.com>
2023-10-23 11:57:29 -0300

    Delete Sistem_banco directory

--- Sistem_banco/src/main/java/enums/UnidadeFederal.java
+++
@@ -1,9 +0,0 @@
-package enums;
-
-public enum UnidadeFederal {
-    RJ, SP, MG, ES, 
-    RS, SC, PR, 
-    BA, SE, PB, PE, RN, CE, MA, PI, AL,
-    RO, RR, AM, AC, TO, PA, AP, 
-    MT, MS, GO, DF;
-}
